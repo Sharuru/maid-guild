e3d264cd8031e42b92d4dba0a92930e9244fa09f
Sharuru <mave@foxmail.com>
2015-11-17 08:58:34 +0000

    框架构建完成

---
+++ src/main/java/com/mbp/MaidGuild/model/TestModel.java
@@ -0,0 +1,23 @@
+package com.mbp.MaidGuild.model;
+
+public class TestModel {
+    Integer recordId;
+
+    public String getRecord() {
+        return record;
+    }
+
+    public void setRecord(String record) {
+        this.record = record;
+    }
+
+    String record;
+
+    public Integer getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(Integer recordId) {
+        this.recordId = recordId;
+    }
+}
