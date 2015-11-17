e95fc1140c09d214a4fe58e5257eeab97894183d
S1507096 <S1507096@8fe30038-93e8-d64f-a5c7-3f0df5b1f493>
2015-11-17 03:31:06 +0000

    Cleanup

--- CustomMaid/Source/MaidGuild/src/main/java/com/mbp/MaidGuild/config/SpringRootConfig.java
+++
@@ -1,9 +0,0 @@
-package com.mbp.MaidGuild.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({ "com.mbp.MaidGuild.service" })
-public class SpringRootConfig {
-}
