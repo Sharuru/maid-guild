766a8d9fa475ee775b2e589d42b6a01d66510cee
Sharuru <mave@foxmail.com>
2015-11-18 02:48:31 +0000

    Temp

---
+++ src/main/java/com/mbp/MaidGuild/utils/APIUtil.java
@@ -0,0 +1,26 @@
+package com.mbp.MaidGuild.utils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+/**
+ * Created by Sharuru on 2015/11/18 0018.
+ */
+public class APIUtil {
+    /**
+     * 发起 Http 请求并捕获返回内容
+     *
+     * @param urlString URL 地址
+     * @return 响应字符串
+     * @throws Exception
+     */
+    public static String readUrl(String urlString) throws Exception {
+        URL url = new URL(urlString);
+        URLConnection con = url.openConnection();
+        InputStream in = con.getInputStream();
+        String encoding = con.getContentEncoding();
+        encoding = encoding == null ? "UTF-8" : encoding;
+        return IOUtils.toString(in, encoding);
+    }
+}
---
+++ src/main/java/com/mbp/MaidGuild/web/StartController.java
@@ -0,0 +1,40 @@
+package com.mbp.MaidGuild.web;
+
+import com.mbp.MaidGuild.service.StartService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Sharuru on 2015/11/18 0018.
+ * 这个 Controller 主要负责在 APP 启动时进行一系列的信息上报功能
+ */
+
+@Controller
+@RequestMapping(value="/start")
+public class StartController {
+
+    //日志记录支持
+    private final Logger logger = LoggerFactory.getLogger(TestController.class);
+    //服务声明
+    private final StartService startService;
+    @Autowired
+    public StartController(StartService startService){this.startService = startService;}
+
+    @RequestMapping(value = "/getCity&location={location}}", method = RequestMethod.GET)
+    @ResponseBody
+    public ModelAndView getCity(ModelAndView model ,@PathVariable("location") String location){
+        //ModelAndView model = new ModelAndView();
+        logger.info("In /getCity! $location:{}",location);
+        startService.getBaiduGeocoderJson(location);
+        System.out.println("in abc");
+        return model;
+    }
+
+}
