94f5b0fbe800f52afef8bca3c552d0820340238f
S1507096 <S1507096@8fe30038-93e8-d64f-a5c7-3f0df5b1f493>
2016-01-04 06:11:17 +0000

    CX002 服务端功能实现

--- CustomMaid/Source/MaidGuild/src/main/java/com/mbp/MaidGuild/service/TravelService.java
+++ CustomMaid/Source/MaidGuild/src/main/java/com/mbp/MaidGuild/service/TravelService.java
@@ -12,6 +12,8 @@ import com.mbp.MaidGuild.model.RoadsModel.RoadsJson;
 import com.mbp.MaidGuild.utils.APIUtil;
 import com.mbp.MaidGuild.utils.MyBatisUtil;
 import com.mbp.MaidGuild.web.TestController;
+import org.apache.commons.io.output.StringBuilderWriter;
+import org.apache.ibatis.reflection.SystemMetaObject;
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,6 +36,8 @@ public class TravelService {
     private final Logger logger = LoggerFactory.getLogger(TestController.class);
     //服务声明
     APIKeyService apiKeyService = new APIKeyService();
+
+    Map<String, String> shMetroMap = new HashMap<>();
 
     public LongDBusJson getLongDBusJson(String from, String to) {
         String jsonStr;
@@ -113,7 +117,6 @@ public class TravelService {
         //拼接请求字符串获得内容
         try {
             jsonStr = APIUtil.readUrl("http://apis.baidu.com/apistore/trafficevent/search?location=北京&output=json", param);
-            //jsonStr = APIUtil.readUrl("http://api.map.baidu.com/telematics/v3/trafficEvent?location=" + URLEncoder.encode(location, "UTF-8") + "&output=json&ak=" + apiKeyService.getUsableAPIKeyByProvider("BAIDUWEB"), null);
             Gson gson = new Gson();
             obj = gson.fromJson(jsonStr, RoadsJson.class);
         } catch (Exception e) {
@@ -132,6 +135,12 @@ public class TravelService {
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
+        //延迟
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         //票价
         ShMetroModel.ShMetroPJson pObj = null;
         try {
@@ -142,28 +151,19 @@ public class TravelService {
         }
         //拼接
         MetroModel.MetroJson obj = new MetroModel.MetroJson();
-        //生成查询词典
-        Map<String, String> shMetroMap = new HashMap<>();
-        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
-        MetroInfoMapper mim = sqlSession.getMapper(MetroInfoMapper.class);
-        List<MetroInfoModel> mimList = mim.selectAllStationByCityId("101020100");
-        for (int i = 0; i < mimList.size(); i++) {
-            shMetroMap.put(mimList.get(i).getStationId(), mimList.get(i).getStationName());
-        }
-        System.out.println(mimList.size());
         //生成 data model
         List<MetroModel.Data> list = new ArrayList<>();
         for (int i = 0; i < cObj.getData().getTotal(); i++) {
             MetroModel.Data data = new MetroModel.Data();
-            data.setDestinationStationId(cObj.getData().getList()[i].getD());
+            data.setDestinationStationName(trimStationIdForShMetro(cObj.getData().getList()[i].getD(), "Single"));
             data.setNo(cObj.getData().getList()[i].getNo());
             data.setInterchangeDuration(cObj.getData().getList()[i].getPttm());
             data.setPassedStationAmount(cObj.getData().getList()[i].getPsnm());
-            data.setOriginStationId(cObj.getData().getList()[i].getO());
+            data.setOriginStationName(trimStationIdForShMetro(cObj.getData().getList()[i].getO(), "Single"));
             data.setOlasttime(cObj.getData().getList()[i].getOlasttime());
-            data.setPassedStationId(cObj.getData().getList()[i].getPsid());
+            data.setPassedStationName(trimStationIdForShMetro(cObj.getData().getList()[i].getPsid(), "Passed"));
             data.setLttm(cObj.getData().getList()[i].getLttm());
-            data.setInterchangeStationId(cObj.getData().getList()[i].getPtid());
+            data.setInterchangeStationName(trimStationIdForShMetro(cObj.getData().getList()[i].getPtid(), "Inter"));
             data.setLstm(cObj.getData().getList()[i].getLstm());
             data.setPassedDuration(cObj.getData().getList()[i].getPstm());
             data.setInterchangeLine(cObj.getData().getList()[i].getPlid());
@@ -175,5 +175,58 @@ public class TravelService {
         return obj;
     }
 
-
+    public String trimStationIdForShMetro(String raw, String trimType) {
+        //查询字典没有数据的场合
+        if (shMetroMap.size() == 0) {
+            SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+            MetroInfoMapper mim = sqlSession.getMapper(MetroInfoMapper.class);
+            List<MetroInfoModel> mimList = mim.selectAllStationByCityId("101020100");
+            for (MetroInfoModel currObj : mimList) {
+                shMetroMap.put(currObj.getStationId(), currObj.getStationName());
+            }
+        }
+        //单一去0:0111 -> 111 -> STATION_NAME
+        switch (trimType) {
+            case "Single":
+                //ASCII(48)="0"
+                if (raw.charAt(0) == 48) {
+                    raw = raw.substring(1);
+                }
+                raw = shMetroMap.get(raw);
+                break;
+            //途径站列:111-112-113 -> STATION_NAME_1-STATION_NAME_2
+            case "Passed": {
+                StringBuilder strb = new StringBuilder();
+                String[] rawArr = raw.split("-");
+                for (String currRaw : rawArr) {
+                    strb.append(shMetroMap.get(currRaw));
+                    strb.append("-");
+                }
+                raw = strb.toString().substring(0, strb.toString().length() - 1);
+                break;
+            }
+            //换乘:111(112)-113(114) -> STATION_NAME_1(STATION_NAME_2)-STATION_NAME_3(STATION_NAME_4)
+            case "Inter": {
+                //匹配出所有站点 ID
+                String regex = "\\d{3,4}";
+                Pattern p = Pattern.compile(regex);
+                Matcher m = p.matcher(raw);
+                StringBuilder strb = new StringBuilder();
+                Boolean appendFlag = true;
+                while (m.find()) {
+                    strb.append(shMetroMap.get(m.group()));
+                    if (appendFlag) {
+                        strb.append("(");
+                        appendFlag = !appendFlag;
+                    } else {
+                        strb.append(")-");
+                        appendFlag = !appendFlag;
+                    }
+                }
+                raw = strb.toString().substring(0, strb.toString().length() - 1);
+                break;
+            }
+        }
+        return raw;
+    }
 }
